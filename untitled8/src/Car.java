b1e8ed274122dfda7b2023733287b977182e210e
syndaly1 <241481@astanait.edu.kz>
2025-09-11 17:29:09 +0500

    Add files via upload

---
+++ untitled8/src/Car.java
@@ -0,0 +1,65 @@
+class Car {
+    private final String color;
+    private final int wheels;
+    private final String engine;
+    private final boolean leatherSeats;
+    private final String style;
+
+
+    private Car(String color,int wheels,String engine,boolean leatherSeats, String style) {
+        this.color = color;
+        this.wheels = wheels;
+        this.engine = engine;
+        this.leatherSeats = leatherSeats;
+        this.style = style;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", wheels=" + wheels +
+                ", engine='" + engine + '\'' +
+                ", leatherSeats=" + leatherSeats +
+                ", style='" + style + '\'' +
+                '}';
+    }
+
+    // вложенный статический класс Builder
+    static class Builder {
+        private String color;
+        private int wheels;
+        private String engine;
+        private boolean leatherSeats;
+        private String style;
+
+        public Builder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder setWheels(int wheels) {
+            this.wheels = wheels;
+            return this;
+        }
+
+        public Builder setEngine(String engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public Builder setLeatherSeats(boolean leatherSeats) {
+            this.leatherSeats = leatherSeats;
+            return this;
+        }
+
+        public Builder setStyle(String style) {
+            this.style = style;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(color, wheels, engine, leatherSeats, style);
+        }
+    }
+}
---
+++ untitled8/src/Main.java
@@ -0,0 +1,29 @@
+//public class Main {
+////    public static void bubbleSort (int[] arr){
+////        int n = arr.length;
+////        for (int i = 0; i < n - 1; i++){
+////            for (int j = 0; j < n - i - 1; j++) {
+////                if (arr[j] > arr[j + 1]) {
+////                    int temp = arr[j];
+////                    arr[j] = arr[j + 1];
+////                    arr[j + 1] = temp;
+////                }
+////            }
+////        }
+////    }
+//
+//}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car.Builder()
+                .setColor("red")
+                .setWheels(4)
+                .setEngine("diesel")
+                .setLeatherSeats(true)
+                .setStyle("sport")
+                .build();
+
+        System.out.println(car);
+    }
+}
